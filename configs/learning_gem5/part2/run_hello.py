f0158d9b164518c703053c59c74e00a64e007336
likmi <likmi@qq.com>
2021-08-07 11:01:25 +0800

    cpu:clean all file
    Change-Id: Ie36e61ab9cf9d40e06a381283f18c6d2696c15bf

--- configs/learning_gem5/part2/run_hello.py
+++ configs/learning_gem5/part2/run_hello.py
@@ -25,9 +25,29 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from m5.params import *
-from m5.SimObject import SimObject
+""" Simple config/run script for the HelloObject
 
-class SimpleObject(SimObject):
-    type = 'SimpleObject'
-    cxx_header = "learning_gem5/part2/simple_object.hh"
+This is probably the simplest gem5 config file you can possibly create.
+It creates a Root object and one *very* simple SimObject and simulates the
+system. Since there are no events, this "simulation" should finish immediately
+
+"""
+
+# import the m5 (gem5) library created when gem5 is built
+import m5
+# import all of the SimObjects
+from m5.objects import *
+
+# set up the root SimObject and start the simulation
+root = Root(full_system = False)
+
+# Create an instantiation of the simobject you created
+root.hello = HelloObject(time_to_wait='2us')
+
+# root.hello.time_to_wait = '2us'
+# instantiate all of the objects we've created above
+m5.instantiate()
+
+print("Beginning simulation!")
+exit_event = m5.simulate()
+print('Exiting @ tick %i because %s' % (m5.curTick(), exit_event.getCause()))
--- configs/tutorial/part1/two_level.py
+++ configs/tutorial/part1/two_level.py
@@ -111,9 +111,10 @@ isa = str(m5.defines.buildEnv['TARGET_ISA']).lower()
 # Default to running 'hello', use the compiled ISA to find the binary
 # grab the specific path to the binary
 thispath = os.path.dirname(os.path.realpath(__file__))
+#binary = os.path.join(thispath, '../../../',
+#                      'tests/test-progs/hello/bin/', isa, 'linux/hello')
 binary = os.path.join(thispath, '../../../',
-                      'tests/test-progs/hello/bin/', isa, 'linux/hello')
-
+                      'cputests/benchmarks/bin', isa, 'BubbleSort')
 system.workload = SEWorkload.init_compatible(binary)
 
 # Create a process for a simple "Hello World" application
